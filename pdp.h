3f6900f94168f0bbc9707e627fe5a756453aaa3b
easykvasha <61687166+easykvasha@users.noreply.github.com>
2020-04-15 21:21:18 +0300

    Create pdp.h

---
+++ pdp.h
@@ -0,0 +1,12 @@
+typedef unsigned char byte; //8 bit
+typedef unsigned short int word; //16 bit
+typedef word Adress; //16 bit
+
+#define MEMSIZE (64*1024)
+
+
+void b_write(Adress adr, byte b);
+byte b_read(Adress adr);
+void w_write(Adress adr, word b);
+word w_read(Adress adr);
+void test_mem();
