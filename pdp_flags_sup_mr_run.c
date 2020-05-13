7cd3bfa4fcc85302e3e4215d781b02ebe08819b7
easykvasha <61687166+easykvasha@users.noreply.github.com>
2020-05-13 17:24:35 +0300

    флаги, run, get_mr и get_sup

---
+++ pdp_flags_sup_mr_run.c
@@ -0,0 +1,185 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "pdp.h"
+#include <string.h>
+#include <stdarg.h>
+
+
+byte mem[MEMSIZE];
+word reg[8];
+int N, Z, C, bt, xo;
+struct SSDD  ss , dd, sup;
+struct SSDD get_mr(word w, int z);
+
+struct Command cmd[] = {
+        {0177777, 0000000, "halt", do_halt, n_pr},
+        {0170000, 0010000, "mov",  do_mov, y_dd | y_ss},
+        {0170000, 0060000, "add",  do_add, y_dd | y_ss},
+        {0170000, 0110000, "movb", do_movb, y_dd | y_ss},
+        {0177000, 0077000, "sob",  do_sob, y_sup},
+        {0017700, 0005000, "clr",  do_clr, y_dd},
+        {0177400, 0000400, "br",   do_br, y_xo},
+        {0177400, 0001400, "beq",  do_beq, y_xo},
+        {0177400, 0100000, "bpl",  do_bpl, y_xo}
+};
+
+
+void Flags (word w) {
+
+    N = (bt ? (w >> 15) : (w >> 7)) & 1;
+    Z = (w == 0);
+    C = (bt ? (w >> 16) : (w >> 8)) & 1;
+
+    if (N == 1)
+        trace (type, "N");
+    if (N != 1)
+        trace (type, "-");
+    if (Z == 1)
+        trace (type, "Z");
+    if (Z != 1)
+        trace (type, "-");
+    if (C == 1)
+        trace (type, "C");
+    if (C != 1)
+        trace (type, "-");
+}
+
+struct SSDD get_sup (word w) {
+    struct SSDD res;
+    res.val = w & 077;
+    trace (type, "R%d, %o", reg[sup.adr], pc - 2 * sup.val);
+    return res;
+}
+
+struct SSDD get_mr(word w, int z) {
+    struct SSDD res;
+    int rgtr = w & 7;					// номер регистра
+    int mode = (w >> 3) & 7;		// номер моды
+    word wd ;
+
+    switch(mode) {
+
+        case 0:
+            res.adr = rgtr;
+            res.val = reg[rgtr];
+            trace (type, "R%o ", rgtr);
+            break;
+
+            case 1:
+                res.adr = reg[rgtr];
+                res.val = z ? b_read(res.adr) : w_read(res.adr);
+                trace (type, "(R%o) ", rgtr);
+                break;
+
+                case 2:
+                    res.adr = reg[rgtr];
+                    res.val = z ? b_read(res.adr) : w_read(res.adr);
+
+                    if (rgtr == 7)
+                        trace (type, "#%o ", res.val);
+                    else
+                        trace (type, "(R%o)+ ", rgtr);
+
+                    if (z == 0 || rgtr == 6 || rgtr == 7)
+                        reg[rgtr] += 2;
+                    else
+                        reg[rgtr]++;
+                    break;
+
+                    case 3:
+                        res.adr = reg[rgtr];
+                        res.adr = w_read(res.adr);
+
+                        if (z == 0 || rgtr == 6 || rgtr == 7){
+                            res.val = w_read(res.adr);
+                        trace (type, "@#%o", res.val);
+                        reg[rgtr] += 2;
+            }else
+                {
+                            res.val = b_read (res.adr);
+                            reg[rgtr] += 2;
+                            trace (type, "@(R%o)+", rgtr);
+            }
+                        break;
+
+                        case 4:
+                            if (z == 0 || rgtr == 6 || rgtr == 7){
+                                reg[rgtr] -= 2;
+                                res.adr = reg[rgtr];
+                                res.val = w_read (res.adr);
+                            }else {
+                                reg[rgtr]--;
+                                res.adr = reg[rgtr];
+                                res.val = b_read (res.adr);
+            }
+                            trace (type, "-(R%d) ", rgtr);
+                            break;
+
+                            case 5:
+                                trace (type, "@-(R%o) ", rgtr);
+                                reg[rgtr] -= 2;
+                                res.adr = reg[rgtr];
+                                res.adr = w_read(res.adr);
+                                res.val = w_read(res.adr);
+                                break;
+
+                                case 6:
+                                    wd= w_read(reg[7]);
+                                    reg[7] += 2;
+                                    res.adr = (reg[rgtr] + wd) & 0xFFFF;
+                                    res.val = w_read(res.adr);
+
+                                    if (rgtr == 7)
+                                        trace (type, "%o ", res.adr);
+                                    else
+                                        trace (type, "%o(R%d) ", wd, rgtr);
+                                    break;
+
+
+                                    default:
+                                        fprintf (stderr, "Mode %o not realised yet\n", mode);
+                                        exit(1);
+    }
+    return res;
+
+}
+
+
+void run() {
+    trace (type, "\n\nBEGIN TRACING\n");
+    pc = 01000;
+    w_write(ostat, 0xFF);
+    trace (type, "\n");
+    while (1) {
+        word w = w_read(pc);
+        trace (type, "%06o :  ", pc);
+        pc = pc + 2;
+        int i;
+        int size = sizeof(cmd)/sizeof(struct Command);
+        struct Command cmmd;
+        for (i = 0; i < size; i++) {
+            cmmd = cmd[i];
+            if ((cmmd.mask & w) == cmmd.opcode) {
+                trace (type, "%s    ", cmmd.name);
+                if (cmmd.pr & y_ss) {
+                    ss = get_mr(w >> 6, w >> 15);
+                    //trace (type, "\n ss = %o, %o\n", ss.val, ss.adr);
+                }
+                if (cmmd.pr & y_dd) {
+                    dd = get_mr(w, w >> 15);
+                    //trace (type, "\n dd = %o, %o\n", dd.val, dd.adr);
+                }
+                if (cmmd.pr & y_sup) {
+                    sup = get_sup(w);
+                    //trace (type, "\n sup = %o, %o\n", sup.val, sup.adr);
+                }
+                if (cmmd.pr & y_xo) {
+                    xo = (char)(w & 255);
+                }
+                cmmd.do_func();
+            }
+        }
+        trace (type, "\n");
+    }
+}
+
