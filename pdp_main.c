162f4c21c86d78e909e6da9e5f38630f7862df12
easykvasha <61687166+easykvasha@users.noreply.github.com>
2020-05-13 17:27:39 +0300

    основной алгоритм

---
+++ pdp_main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "pdp.h"
+#include <string.h>
+#include <stdarg.h>
+
+int main(int arg, char * argm[]) {
+    if (arg == 1) {
+        settings(argm[0]);
+        exit(1);
+    }
+
+    for (int i = 1; i < arg-1; i++) {
+        if (0 == strcmp(argm[i], "-t"))
+          type = 1;
+
+        else {
+            printf("Option can't be done\n");
+            settings(argm[0]);
+            exit(1);
+        }
+    }
+    load_file(argm[arg-1]);
+    run();
+}
