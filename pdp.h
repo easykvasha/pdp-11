38509d5dde977175269ae99ee49b0c294d8b1b6a
easykvasha <61687166+easykvasha@users.noreply.github.com>
2020-05-14 21:18:24 +0300

    Update pdp.h

--- pdp.h
+++ pdp.h
@@ -1,6 +1,4 @@
-typedef unsigned char byte; //8 bit
-typedef unsigned short int word; //16 bit
-typedef word Adress; //16 bit
+
 
 typedef unsigned char byte; //8 bit
 typedef unsigned short int word; //16 bit
